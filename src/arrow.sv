063f5a4d896f8cd797025c60a4a3ac752cdccc78
builder <builder@EECS-DIGITAL-22.MIT.EDU>
2022-11-07 18:54:20 -0500

    made arrow

---
+++ src/arrow.sv
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module arrow #(parameter WIDTH = 8,HEIGHT = 32)(
+	input wire clk,
+	input wire[10:0] hcount_in,
+	input wire[9:0] vcount_in,
+	input wire valid_in,
+	input wire[2:0] speed_in,
+	input wire[1:0] direction_in,
+	input wire inversed_in,
+
+	output logic[11:0] pixel_out,
+	output logic valid_out
+);
+
+logic[10:0] x;
+logic[9:0] y;
+logic old_valid_in;
+
+always_ff @(posedge clk)begin
+	if(~old_valid_in && valid_in)begin
+		case(direction_in)
+			2'b00:begin
+				x <= 512;
+				y <= 0;
+			end
+			2'b01:begin
+				x <= 512;
+				y <= 0;
+			end
+			2'b10:begin
+				x <= 512;
+				y <= 0;
+			end
+			2'b11:begin
+				x <= 512;
+				y <= 0;
+			end
+		endcase
+	end else if(old_valid_in && valid_in)begin
+		if(hcount_in == 0 && vcount_in == 0)begin
+			case(direction_in)
+                        	2'b00:begin
+                                	y <= y + 4;
+                        	end
+                        	2'b01:begin
+                                	y <= y + 4;
+                        	end
+                        	2'b10:begin
+                                	y <= y + 4;
+                        	end
+                        	2'b11:begin
+                                	y <= y + 4;
+                        	end
+			endcase
+
+
+		end
+
+	end
+	old_valid_in <= valid_in;
+end
+assign valid_out = (hcount_in >= x) && (hcount_in <= x+WIDTH) && (vcount_in >= y) && (vcount_in <= y+HEIGHT);
+assign pixel_out = valid_out ? 12'hF00 : 0;
+endmodule
+`default_nettype wire
+
--- src/enemy.sv
+++ src/enemy.sv
@@ -17,19 +17,24 @@ module enemy(
 logic busy_out_buffer;
 
 //patterns
-//pattern pattern1#(72'hFFFF_FFFF_FFFF_FFFF_FF,72'h249_249_249_249_249_249,48'h)
+//pattern pattern1#(72'hFFFF_FFFF_FFFF_FFFF_FF,72'h249_249_249_249_249_249,48'h0,24'h0,0)
 
+
+//logic[23:0] arrow_on;
 //24 arrows
+logic[11:0] arrow_out1;
+logic arrow_valid1;
+arrow #(8,32) arrow1(.clk(clk),.hcount_in(hcount_in),.vcount_in(vcount_in),.valid_in(1),.speed_in(0),.direction_in(2'b00),.inversed_in(0),.pixel_out(arrow_out1),.valid_out(arrow_valid1));
 
 logic frame_top_out,frame_bottom_out,frame_right_out,frame_left_out;
 logic[11:0] frame_top_pixel;
 logic[11:0] frame_bottom_pixel;
 logic[11:0] frame_right_pixel;
 logic[11:0] frame_left_pixel;
-block_sprite #(128,16,256,256,12'hFFFFFF) frame_top(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.pixel_out(frame_top_pixel),.in_sprite(frame_top_out));
-block_sprite #(16,128,256,256,12'hFFFFFF) frame_left(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_left_out),.pixel_out(frame_left_pixel));
-block_sprite #(16,128,368,256,12'hFFFFFF) frame_right(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_right_out),.pixel_out(frame_right_pixel));
-block_sprite #(128,16,256,368,12'hFFFFFF) frame_bottom(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_bottom_out),.pixel_out(frame_bottom_pixel));
+block_sprite #(160,8,432,304,12'hFFF) frame_top(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.pixel_out(frame_top_pixel),.in_sprite(frame_top_out));
+block_sprite #(8,160,432,304,12'hFFF) frame_left(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_left_out),.pixel_out(frame_left_pixel));
+block_sprite #(8,1160,584,304,12'hFFF) frame_right(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_right_out),.pixel_out(frame_right_pixel));
+block_sprite #(160,8,432,456,12'hFFF) frame_bottom(.is_fixed(1),.x_in(0),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.in_sprite(frame_bottom_out),.pixel_out(frame_bottom_pixel));
 
 always_comb begin
 	if(busy_out_buffer)begin
@@ -41,7 +46,7 @@ always_comb begin
                 	pixel_out = frame_right_pixel;
         	if(frame_left_out)
                 	pixel_out = frame_left_pixel;*/
-		pixel_out =frame_bottom_pixel + frame_top_pixel + frame_left_pixel + frame_right_pixel;
+		pixel_out =frame_bottom_pixel + frame_top_pixel + frame_left_pixel + frame_right_pixel + arrow_out1;
 	end
 end
 
@@ -55,6 +60,9 @@ always_ff @(posedge clk)begin
 	end else begin
 		if(state_in == 4'b1000)begin
 			busy_out_buffer <= 1;
+		end
+		if(busy_out_buffer)begin
+
 		end
 	end
 
