cb6f29b1f1be63e89506d4401d55f0ce9c57cdc0
builder <builder@EECS-DIGITAL-22.MIT.EDU>
2022-12-08 18:58:11 -0500

    HP font does not show up why

--- src/fonts.sv
+++ src/fonts.sv
@@ -30,23 +30,141 @@ assign g_bram[10] = 12'hFFF;
 assign g_bram[11] = 12'h7FF;
 
 /*pixel filling for "A"*/
-logic[11:0] a_bram[11:0]
-assign a_bram[0] = 12'h7FF;
+logic[11:0] a_bram[11:0];
+assign a_bram[0] = 12'h7FE;
 assign a_bram[1] = 12'hFFF;
-assign a_bram[2] = 12'hF00;
-assign a_bram[3] = 12'hF00;
-assign a_bram[4] = 12'hF3F;
-assign a_bram[5] = 12'hF3F;
-assign a_bram[6] = 12'hF3F;
-assign a_bram[7] = 12'hF0F;
-assign a_bram[8] = 12'hF0F;
-assign a_bram[9] = 12'hF0F;
-assign a_bram[10] = 12'hFFF;
-assign a_bram[11] = 12'h7FF;
+assign a_bram[2] = 12'hF9F;
+assign a_bram[3] = 12'hF9F;
+assign a_bram[4] = 12'hF9F;
+assign a_bram[5] = 12'hFFF;
+assign a_bram[6] = 12'hFFF;
+assign a_bram[7] = 12'hF9F;
+assign a_bram[8] = 12'hF9F;
+assign a_bram[9] = 12'hF9F;
+assign a_bram[10] = 12'hF9F;
+assign a_bram[11] = 12'hF9F;
 
+/*pixel fillin for "M"*/
+logic[17:0] m_bram[11:0];
+assign m_bram[0]  = 18'b111111111111111110;
+assign m_bram[1]  = 18'b111111111111111111;
+assign m_bram[2]  = 18'b111100011110001111;
+assign m_bram[3]  = 18'b111100011110001111;
+assign m_bram[4]  = 18'b111100011110001111;
+assign m_bram[5]  = 18'b111100011110001111;
+assign m_bram[6]  = 18'b111100011110001111;
+assign m_bram[7]  = 18'b111100011110001111;
+assign m_bram[8]  = 18'b111100011110001111;
+assign m_bram[9]  = 18'b111100011110001111;
+assign m_bram[10] = 18'b111100011110001111;
+assign m_bram[11] = 18'b111100011110001111;
+
+/*pixel filling for "E"*/
+logic[11:0] e_bram[11:0];
+assign e_bram[0]  = 12'h7FF;
+assign e_bram[1]  = 12'hFFF;
+assign e_bram[2]  = 12'hF00;
+assign e_bram[3]  = 12'hF00;
+assign e_bram[4]  = 12'hF00;
+assign e_bram[5]  = 12'hFF0;
+assign e_bram[6]  = 12'hFF0;
+assign e_bram[7]  = 12'hF00;
+assign e_bram[8]  = 12'hF00;
+assign e_bram[9]  = 12'hF00;
+assign e_bram[10] = 12'hFFF;
+assign e_bram[11] = 12'h7FF;
+
+/*pixel filling for "O"*/
+logic[11:0] o_bram[11:0];
+assign o_bram[0] = 12'h7FE;
+assign o_bram[1] = 12'hFFF;
+assign o_bram[2] = 12'hF9F;
+assign o_bram[3] = 12'hF9F;
+assign o_bram[4] = 12'hF9F;
+assign o_bram[5] = 12'hF9F;
+assign o_bram[6] = 12'hF9F;
+assign o_bram[7] = 12'hF9F;
+assign o_bram[8] = 12'hF9F;
+assign o_bram[9] = 12'hF9F;
+assign o_bram[10] = 12'hFFF;
+assign o_bram[11] = 12'h7FE;
+
+/*pixel filling for "V"*/
+logic[11:0] v_bram[11:0];
+assign v_bram[0] = 12'hF1F;
+assign v_bram[1] = 12'hF1F;
+assign v_bram[2] = 12'hF1F;
+assign v_bram[3] = 12'hF1F;
+assign v_bram[4] = 12'hF1F;
+assign v_bram[5] = 12'hF1F;
+assign v_bram[6] = 12'hF1F;
+assign v_bram[7] = 12'hF1F;
+assign v_bram[8] = 12'hF1F;
+assign v_bram[9] = 12'hF1F;
+assign v_bram[10] = 12'hFFF;
+assign v_bram[11] = 12'hFFE;
+
+/*pixel filling for "R"*/
+logic[11:0] r_bram[11:0];
+assign r_bram[0]  = 12'hFFE;
+assign r_bram[1]  = 12'hFFF;
+assign r_bram[2]  = 12'hF1F;
+assign r_bram[3]  = 12'hF1F;
+assign r_bram[4]  = 12'hF1F;
+assign r_bram[5]  = 12'hFFE;
+assign r_bram[6]  = 12'hFFE;
+assign r_bram[7]  = 12'hF1F;
+assign r_bram[8]  = 12'hF1F;
+assign r_bram[9]  = 12'hF1F;
+assign r_bram[10] = 12'hF1F;
+assign r_bram[11] = 12'hF1F;
+
+/*pixel filling for "H"*/
+logic[11:0] h_bram[11:0];
+assign h_bram[0]  = 12'hF0F;
+assign h_bram[1]  = 12'hF0F;
+assign h_bram[2]  = 12'hF0F;
+assign h_bram[3]  = 12'hF0F;
+assign h_bram[4]  = 12'hF0F;
+assign h_bram[5]  = 12'hFFF;
+assign h_bram[6]  = 12'hFFF;
+assign h_bram[7]  = 12'hF0F;
+assign h_bram[8]  = 12'hF0F;
+assign h_bram[9]  = 12'hF0F;
+assign h_bram[10] = 12'hF0F;
+assign h_bram[11] = 12'hF0F;
+
+/*pixel filling for "P"*/
+logic[11:0] p_bram[11:0];
+assign p_bram[0]  = 12'hF0F;
+assign p_bram[1]  = 12'hF0F;
+assign p_bram[2]  = 12'hF0F;
+assign p_bram[3]  = 12'hF0F;
+assign p_bram[4]  = 12'hF0F;
+assign p_bram[5]  = 12'hFFF;
+assign p_bram[6]  = 12'hFFF;
+assign p_bram[7]  = 12'hF0F;
+assign p_bram[8]  = 12'hF0F;
+assign p_bram[9]  = 12'hF0F;
+assign p_bram[10] = 12'hF0F;
+assign p_bram[11] = 12'hF0F;
+
+
+
+/*
+* 00001 = G
+* 00010 = A
+* 00011 = M
+* 00100 = E
+* 00101 = O
+* 00110 = V
+* 00111 = R
+* 01000 = H
+* 01001 = P
+*/
 always_comb begin
 	case(letter_in)
-		4'b0001:begin
+		5'b00001:begin
 			relative_x <= hcount_in - X_POS;
                 	relative_y <= vcount_in - Y_POS;
                 	if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
@@ -58,6 +176,102 @@ always_comb begin
                 	else
                         	in_sprite_buffer = 0;
 		end
+		5'b00010:begin
+			relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = a_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+		end
+		5'b00011:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 18*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 17 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = m_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b00100:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = e_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b00101:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = o_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b00110:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = v_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b00111:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = r_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b01000:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = h_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
+		5'b01001:begin
+                        relative_x <= hcount_in - X_POS;
+                        relative_y <= vcount_in - Y_POS;
+                        if(relative_x >= 0 && relative_x <= 12*(2**scale_in)-1 && relative_y >= 0 && relative_y <= 12*(2**scale_in)-1)begin
+                                modified_x = 11 - (relative_x >> scale_in);
+                                modified_y = relative_y >> scale_in;
+                                in_sprite_buffer = p_bram[modified_y][modified_x];
+                                //in_sprite_buffer = pixel_bram[3][0];
+                        end
+                        else
+                                in_sprite_buffer = 0;
+                end
 	endcase
        /*relative_x <= hcount_in - X_POS;
        relative_y <= vcount_in - Y_POS;
--- src/game_state.sv
+++ src/game_state.sv
@@ -38,6 +38,12 @@ logic undyne_out;
 logic[10:0] undyne_x;
 image_sprite #(372,372,"undyne.mem","undyne_palette.mem") undyne(.pixel_clk_in(clk),.rst_in(rst),.x_in(undyne_x),.hcount_in(hcount_in),.y_in(0),.vcount_in(vcount_in),.pixel_out(undyne_pixel_out),.in_sprite(undyne_out));
 
+logic[11:0] hp_pixel_out[1:0];
+logic[1:0] hp_out;
+
+fonts #(424,584) H(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b01000),.color_in(12'hFFF),.scale_in(2),.pixel_out(hp_pixel_out[0]),.in_sprite(hp_out[0]));
+fonts #(452,584) P(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b01000),.color_in(12'hFFF),.scale_in(2),.pixel_out(hp_pixel_out[1]),.in_sprite(hp_out[1]));
+
 enemy en(
 	.clk(clk),
 	.rst(state_rst),
@@ -87,7 +93,6 @@ logic player_finish_out,old_player_finish_out;
 logic[11:0] player_pixel_out;
 logic game_over_busy_out;
 logic game_over_finish_out,old_game_over_finish_out;
-logic[11:0] game_over_pixel_out;
 
 /*game_over go(
 	.clk(clk),
@@ -113,33 +118,49 @@ health_bar #(480,584,96,32) hb(
 /*variables for game_over*/
 logic[31:0] timing_count;
 logic[2:0] animation_phase;
-logic[11:0] green_heart_pixel_out,g_pixel_out;
-logic green_heart_out,g_out;
+logic[11:0] green_heart_pixel_out;
+logic[11:0] game_over_pixel_out[7:0];
+logic[7:0] font_out;
+logic green_heart_out;
 logic divided_in;
 logic fall_apart_valid_in;
 logic[11:0] fall_apart_pixel_out;
 logic[11:0] fall_apart_out;
+logic[3:0] color_r_in,color_g_in,color_b_in;
+logic[11:0] font_color_in;
+logic[7:0] fade_interval;
 green_heart_sprite #(496,368,12'h0F0) green_heart(.hcount_in(hcount_in),.vcount_in(vcount_in),.divided_in(divided_in),.pixel_out(green_heart_pixel_out),.in_sprite(green_heart_out));
-fonts #(128,128) G(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(4'b0001),.color_in(12'hFFF),.scale_in(2),.pixel_out(g_pixel_out),.in_sprite(g_out));
+fonts #(96,128) G(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00001),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[0]),.in_sprite(font_out[0]));
+fonts #(304,128) A(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00010),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[1]),.in_sprite(font_out[1]));
+fonts #(512,128) M(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00011),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[2]),.in_sprite(font_out[2]));
+fonts #(720,128) E(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00100),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[3]),.in_sprite(font_out[3]));
+fonts #(112,336) O(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00101),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[4]),.in_sprite(font_out[4]));
+fonts #(320,336) V(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00110),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[5]),.in_sprite(font_out[5]));
+fonts #(528,336) E_two(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00100),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[6]),.in_sprite(font_out[6]));
+fonts #(736,336) R(.hcount_in(hcount_in),.vcount_in(vcount_in),.letter_in(5'b00111),.color_in(font_color_in),.scale_in(3),.pixel_out(game_over_pixel_out[7]),.in_sprite(font_out[7]));
+
 
 heart_fall_apart #(12'h0F0) heart_parts(.clk(clk),.rst(rst),.hcount_in(hcount_in),.vcount_in(vcount_in),.valid_in(fall_apart_valid_in),.in_sprite(fall_apart_out),.pixel_out(fall_apart_pixel_out));
 always_comb begin
 	case(state_out)
 		4'b1000:begin
-			pixel_out = enemy_pixel_out + health_bar_pixel_out + undyne_pixel_out;
+			pixel_out = enemy_pixel_out + health_bar_pixel_out + undyne_pixel_out + hp_pixel_out[0] + hp_pixel_out[1];
 		end
 		4'b0000:begin
-			pixel_out = menu_pixel_out + health_bar_pixel_out + undyne_pixel_out;
+			pixel_out = menu_pixel_out + health_bar_pixel_out + undyne_pixel_out + hp_pixel_out[0] + hp_pixel_out[1];
 		end
 		4'b0001:begin
-			pixel_out = player_pixel_out + health_bar_pixel_out + undyne_pixel_out;
+			pixel_out = player_pixel_out + health_bar_pixel_out + undyne_pixel_out + hp_pixel_out[0] + hp_pixel_out[1];
 		end
 		4'b1111:begin
 			if(animation_phase == 1 || animation_phase == 0)begin
-				pixel_out = green_heart_pixel_out + g_pixel_out;
+				pixel_out = green_heart_pixel_out;
 			end
 			else if(animation_phase == 2)begin
-				pixel_out = fall_apart_pixel_out + g_pixel_out;
+				pixel_out = fall_apart_pixel_out;
+			end
+			else if(animation_phase == 3)begin
+				pixel_out = fall_apart_pixel_out + game_over_pixel_out[0] + game_over_pixel_out[1] + game_over_pixel_out[2] + game_over_pixel_out[3] + game_over_pixel_out[4] + game_over_pixel_out[5] + game_over_pixel_out[6] + game_over_pixel_out[7];
 			end
 		end
 	endcase
@@ -147,6 +168,7 @@ end
 
 always_ff @(posedge clk)begin
 	if(rst)begin
+		//game_over_out <= 0;
 		state_out <= 4'b0000;
 		old_enemy_finish_out <= 0;
 		old_menu_finish_out <= 0;
@@ -155,6 +177,10 @@ always_ff @(posedge clk)begin
 		round_rst <= 0;
 		fall_apart_valid_in <= 0;
 		timing_count <= 0;
+		color_r_in <= 0;
+		color_g_in <= 0;
+		color_b_in <= 0;
+		fade_interval <= 0;
 	end
 	else if(round_rst)begin
 		state_out <= 4'b0000;
@@ -165,11 +191,13 @@ always_ff @(posedge clk)begin
 		round_rst <= 0;
 		fall_apart_valid_in <= 0;
 		timing_count <= 0;
+		color_r_in <= 0;
+		color_g_in <= 0;
+		color_b_in <= 0;
 	end
 	else begin
 		if(old_game_over_out != game_over_out && game_over_out == 1)begin
 			state_out <= 4'b1111;
-			game_over_pixel_out <= 0;
 			animation_phase <= 0;
 			timing_count <= 0;	
 			divided_in <= 0;
@@ -189,7 +217,7 @@ always_ff @(posedge clk)begin
 		end
 
 		if(state_out <= 4'b1111)begin
-			if(animation_phase == 0)begin
+			if(animation_phase == 0)begin //idling
 				//game_over_pixel_out <= 12'h0F0;
 				timing_count <= timing_count + 1;
 				if(timing_count == 6500000*10)begin
@@ -197,7 +225,7 @@ always_ff @(posedge clk)begin
 					animation_phase <= 1;
 				end
 			end
-			else if(animation_phase == 1)begin
+			else if(animation_phase == 1)begin //heart breaks in two
 				//game_over_pixel_out <= 12'hF00;
 				divided_in <= 1;
 				timing_count <= timing_count + 1;
@@ -206,8 +234,24 @@ always_ff @(posedge clk)begin
 					animation_phase <= 2;
 				end
 			end
-			else if(animation_phase == 2)begin
+			else if(animation_phase == 2)begin //heart fall apart
 				fall_apart_valid_in <= 1;
+				timing_count <= timing_count + 1;
+				if(timing_count == 65000000)begin
+					animation_phase <= 3;
+				end
+			end
+			else if(animation_phase == 3)begin
+				if(hcount_in == 0 && vcount_in == 0)begin
+					fade_interval <= fade_interval + 1;
+					if(fade_interval[5:0] == 6'b11111111)begin
+						if(color_r_in != 4'b1111)begin
+                                                	color_r_in <= color_r_in + 1;
+                                                	color_g_in <= color_g_in + 1;
+                                                	color_b_in <= color_b_in + 1;
+                                        	end
+					end
+				end
 			end
 		end
 		old_enemy_finish_out <= enemy_finish_out;
@@ -216,6 +260,6 @@ always_ff @(posedge clk)begin
 		old_game_over_out <= game_over_out;
 	end
 end
-
+assign font_color_in = {color_r_in,color_g_in,color_b_in};
 endmodule
 `default_nettype wire
